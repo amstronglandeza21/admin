469c19ce6fb9ce0a019fae333ad80e298c97fb51
Luiese Amstrong <amstronglandeza21@gmail.com>
2024-05-16 00:57:34 +0800

    updated commit

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,14 +3,12 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Pending from './Pending';
 
-
 const AppRouter = () => {
   return (
-    <Router>
+    <Router basename="/admin">
       <Routes>
-        <Route path="/" element={<App />}  exact />
+        <Route path="/" element={<App />} />
         <Route path="/pending" element={<Pending />} />
-
       </Routes>
     </Router>
   );
