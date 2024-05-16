a29587448e073da5046a058377b13d58bad6a63b
Luiese Amstrong <amstronglandeza21@gmail.com>
2024-05-16 15:08:32 +0800

    initial commit

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Pending from './Pending';
+import NotFound from './NotFound'; 
 
 const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/pending" element={<Pending />} />
+        <Route path="*" element={<NotFound />} /> 
       </Routes>
     </Router>
   );
